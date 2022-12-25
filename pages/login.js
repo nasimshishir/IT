de55890e80c7b7c5c7df5983ccfc0de08b5f2167
nasimshishir <tech17196@gmail.com>
2022-12-25 12:18:23 +0600

    Login validation

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,44 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 const Login = () => {
    const [error, setError] = useState('');
+   const [user, setUser] = useState({});
+   const router = useRouter();
+
+
+   const userLogin = (e) => {
+      e.preventDefault();
+
+      const email = e.target.email.value;
+      const password = e.target.password.value;
+
+      fetch("http://localhost:5000/login", {
+         method: 'POST',
+         headers: {
+            "content-type": "application/json"
+         },
+         body: JSON.stringify({ email, password })
+
+      })
+         .then(res => res.json())
+         .then(data => {
+            console.log(data);
+            if (data.success) {
+               setUser(data.user);
+               localStorage.setItem('accessToken', data.accessToken);
+               router.push('/products')
+            } else {
+               setError(data.message)
+            }
+         })
+         .catch(e => {
+            console.log(e);
+            setError(e.message)
+         })
+   }
 
    return (
       <>
@@ -12,7 +47,7 @@ const Login = () => {
          </Head>
          <div className='container mx-auto flex justify-center items-center min-h-screen'>
             <div>
-               <form className='border rounded-lg shadow-lg p-10'>
+               <form onSubmit={ userLogin } className='border rounded-lg shadow-lg p-10'>
                   <h1 className="text-4xl font-bold text-center px-24">Login</h1>
                   <div className="my-5">
 
@@ -24,7 +59,7 @@ const Login = () => {
                   </div>
                   {
                      error && <div className='my-5 text-center text-red-600 font-medium'>
-                        <p>{ error.slice(22, -2) }</p>
+                        <p>{ error }</p>
                      </div>
 
                   }
